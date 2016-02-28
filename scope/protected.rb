b6a60aab38746e35612743241fd18579950b4f79
Paul Stivers <paul.stivers@fidelitylife.com>
2016-02-28 12:14:50 -0800

    first commit

---
+++ scope/protected.rb
@@ -0,0 +1,22 @@
+class Person
+  def initialize(age)
+    @age = age
+  end
+
+  def age
+    @age
+  end
+
+  def age_difference_with(other_person)
+    (self.age - other_person.age).abs
+  end
+
+  protected :age
+end
+
+fred = Person.new(34)
+chris = Person.new(25)
+puts chris.age_difference_with(fred)
+
+# this won't work with protected: age
+# puts chris.age
